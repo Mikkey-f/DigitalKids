1fdfe3ebfd99fc75630b935978681355ae4e6ecb
Mikkeyf <1647228132@qq.com>
2025-03-12 19:40:26 +0800

    完成SpringSecurity相关配置

--- digital-common/src/main/java/com/digital/enums/ResultErrorEnum.java
+++ digital-common/src/main/java/com/digital/enums/ResultErrorEnum.java
@@ -13,6 +13,8 @@ public enum ResultErrorEnum {
     PHONE_NUMBER_IS_REPEAT(103, "电话号码已经注册"),
     NEED_RIGHT_AUTH(104, "权限不足"),
     NO_AUTH_ERROR(105, "认证失败"),
+    TOKEN_EXPIRED(106, "token过期"),
+    TOKEN_ERROR(107, "会话错误"),
     SUCCESS(500, "成功!");
     Integer code;
     String message;
--- digital-service/src/main/java/com/digital/handler/AccessDeniedHandlerImpl.java
+++ digital-service/src/main/java/com/digital/handler/AccessDeniedHandlerImpl.java
@@ -6,6 +6,7 @@ import com.digital.result.Result;
 import com.digital.utils.WebUtils;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,7 @@ import java.io.IOException;
  * @Author: Mikkeyf
  * @CreateTime: 2025-03-12  18:55
  */
+@Component
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
 
     @Override
