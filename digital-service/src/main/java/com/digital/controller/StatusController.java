9163e5a1ab63b79227cfe9c1d2103fd3c35cc527
Mikkeyf <1647228132@qq.com>
2025-04-02 16:38:45 +0800

    加入内置md转为html

--- digital-service/src/main/java/com/digital/controller/StatusController.java
+++ digital-service/src/main/java/com/digital/controller/StatusController.java
@@ -5,6 +5,7 @@ import com.digital.constant.UserConstant;
 import com.digital.enums.ResultErrorEnum;
 import com.digital.enums.RoleEnum;
 import com.digital.result.Result;
+import com.digital.utils.MarkdownUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ public class StatusController {
     @GetMapping("/status/{sessionId}")
     public Result<String> getStatus(@PathVariable String sessionId) {
         String s = sessions.get(sessionId);
+        s = MarkdownUtils.markdownToHtml(s);
         if (s == null) {
             Result.error(ResultErrorEnum.QUESTION_RESULT_NOT_FIND.getMessage());
         }
