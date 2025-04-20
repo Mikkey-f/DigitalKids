896b0dd5d4a8c84ebeceed79e76da6e17874c6f5
Mikkeyf <1647228132@qq.com>
2025-04-20 14:27:43 +0800

    threejs model判断工具类

--- digital-common/src/main/java/com/digital/constant/ThreeJsConstant.java
+++ digital-common/src/main/java/com/digital/constant/ThreeJsConstant.java
@@ -6,8 +6,17 @@ package com.digital.constant;
  * threejs模块的常量
  */
 public class ThreeJsConstant {
-    private static final String LittleHigh = "偏高";
-    private static final String LittleLow = "偏低";
-    private static final String VeryHigh = "过高";
-    private static final String VeryLow = "过低";
+    public static final String High = "偏高";
+    public static final String Low = "偏低";
+    public static final String Good = "良好";
+    public static final String Long = "偏长";
+    public static final String Short = "偏短";
+    public static final String LeftEyeStatus = "左眼视力状态";
+    public static final String RightEyeStatus = "右眼视力状态";
+    public static final String HeartBeatRateStatus = "心率状态";
+    public static final String BmiStatus = "bmi状态";
+    public static final String LeftArmStatus = "左臂状态";
+    public static final String LeftLegStatus = "左腿状态";
+    public static final String RightArmStatus = "右臂状态";
+    public static final String RightLegStatus = "右腿状态";
 }
